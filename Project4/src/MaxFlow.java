2adcc93231e3b11b65101ef9acc7e906f0987461
harsha_mohite@outlook.com <hmohite@purdue.edu>
2022-04-10 15:30:42 -0400

    All local test cases passed.

--- Project4/src/MaxFlow.java
+++ Project4/src/MaxFlow.java
@@ -14,7 +14,10 @@ public class MaxFlow
     */
     public MaxFlow(int N)
     {
-        
+        this.N = N + 2;
+        this.adj_list = new HashMap<>();
+        this.parent = new int[this.N];
+        Arrays.fill(this.parent, -1);
     }
 
     /** TODO
@@ -29,7 +32,47 @@ public class MaxFlow
     */
     public void insEdge(int source, int destination, int flow_rate)
     {
-        
+        Edge newEdge = new Edge(source, destination, flow_rate);
+        Edge reverseEdge = new Edge(newEdge.destination, newEdge.source, 0);
+
+        initializeArrayList(source);
+        // make sure there's no duplicate
+        boolean edgeExists = false;
+        for (Edge compareEdge : adj_list.get(source)) {
+            if ((compareEdge.source == newEdge.source)
+                && (compareEdge.destination == newEdge.destination)) {
+                edgeExists = true;
+            }
+        }
+        if (!edgeExists) { // if no duplicate, add
+            adj_list.get(source).add(newEdge);
+        }
+
+        // do the same in the other direction
+        initializeArrayList(destination);
+        boolean reverseEdgeExists = false;
+        for (Edge compareEdge : adj_list.get(destination)) {
+            if ((compareEdge.source == reverseEdge.source)
+                && (compareEdge.destination == compareEdge.destination)) {
+                reverseEdgeExists = true;
+            }
+        }
+        if (!reverseEdgeExists) {
+            adj_list.get(destination).add(reverseEdge);
+        }
+
+    }
+
+    /**
+     * Helper function to check if an ArrayList is initialized, and if not,
+     * initialize it.
+     * @param index Index in the adjacency list to check.
+     */
+    public void initializeArrayList(int index) {
+        if (adj_list.get(index) == null) {
+
+            adj_list.put(index, new ArrayList<>());
+        }
     }
 
     /** TODO
@@ -39,6 +82,28 @@ public class MaxFlow
     */
     boolean bfs()
     {
+        Arrays.fill(this.parent, -1);
+
+        Queue<Integer> Q = new LinkedList<Integer>(); // queue of size |V|
+        Q.add(0); // enqueue v
+        boolean[] visited = new boolean[N];
+        Arrays.fill(visited, false);
+        visited[0] = true; // mark v as visited
+
+        while (!Q.isEmpty()) {
+            int u = Q.remove();
+            for (Edge adjacentEdge : adj_list.get(u)) {
+                int w = adjacentEdge.destination;
+                if (!visited[w] && (adjacentEdge.flow_rate > 0)) {
+                    Q.add(w);
+                    visited[w] = true;
+                    parent[w] = u;
+                }
+            }
+        }
+        if (parent[N-1] != -1) {
+            return true;
+        }
         return false;
     }
 
@@ -55,8 +120,44 @@ public class MaxFlow
     */
     int pathAugmentation()
     {
+        int pond = 0;
+        int barrel = N - 1;
+        int maxflow = 0;
+
+        while (bfs()) {
+            int flow = Integer.MAX_VALUE;
+            ArrayList<Edge> P = getPath();
+            for (Edge e : P) {
+                if (e.flow_rate < flow) {
+                    flow = e.flow_rate;
+                }
+            }
+            for (Edge e : P) {
+                setFlow(e.source, e.destination, e.flow_rate - flow);
+                setFlow(e.destination, e.source, getFlow(e.destination, e.source) + flow);
+            }
+            maxflow += flow;
+        }
+
         /*default value provided*/
-        return 0;
+        return maxflow;
+    }
+
+    public ArrayList<Edge> getPath() {
+        Stack<Edge> reversePathStack = new Stack<Edge>();
+        ArrayList<Edge> pathList = new ArrayList<>();
+        int currentVertex = N - 1;
+        int parentVertex = parent[currentVertex];
+        while (currentVertex != 0) {
+            Edge nextEdge = new Edge(parentVertex, currentVertex, getFlow(parentVertex, currentVertex));
+            reversePathStack.push(nextEdge);
+            currentVertex = parentVertex;
+            parentVertex = parent[parentVertex];
+        }
+        while (!reversePathStack.isEmpty()) {
+            pathList.add(reversePathStack.pop());
+        }
+        return pathList;
     }
 
     /** TODO
@@ -69,6 +170,11 @@ public class MaxFlow
     */
     int getFlow(int source, int destination)
     {
+       for (Edge flowEdge : adj_list.get(source)) {
+           if ((flowEdge.source == source) && (flowEdge.destination == destination)) {
+               return flowEdge.flow_rate;
+           }
+       }
        return 0;
     }
 
@@ -81,14 +187,27 @@ public class MaxFlow
     */
     void setFlow(int source, int destination, int flow_rate)
     {
-       
+       boolean edgeFound = false;
+
+       for (Edge flowEdge : adj_list.get(source)) {
+           if ((flowEdge.source == source) && (flowEdge.destination == destination)) {
+               flowEdge.flow_rate = flow_rate;
+               edgeFound = true;
+               return;
+           }
+       }
+
+       if (edgeFound == false) {
+           Edge newEdge = new Edge(source, destination, flow_rate);
+           adj_list.get(source).add(newEdge);
+       }
     }
 
     public static void main(String []args)
     {
         try {
             MaxFlow obmax = new MaxFlow(0);
-            File myObj = new File("./src/com/cs251/sampleMaxFlowData.txt");
+            File myObj = new File("./src/sampleMaxFlowData.txt");
             Scanner myReader = new Scanner(myObj);
             int line = 0;
             while (myReader.hasNextLine()) {
